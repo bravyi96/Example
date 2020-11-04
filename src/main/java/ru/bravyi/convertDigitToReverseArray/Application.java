ff7121e89500353b7138ea4a9fcfe8cc355f35bc
bravyi96 <maksim.bravyi@yandex.ru>
2020-11-04 12:31:28 +0300

    ConvertDigitToReverseArray v2

--- src/main/java/ru/bravyi/convertDigitToReverseArray/Application.java
+++ src/main/java/ru/bravyi/convertDigitToReverseArray/Application.java
@@ -4,15 +4,15 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        int digit = 348597;
-        int[] array = convertDigitToReverseArray(Integer.toString(digit));
+        long digit = 348597;
+        int[] array = convertDigitToReverseArray(Long.toString(digit));
         System.out.println(Arrays.toString(array));
     }
 
     private static int[] convertDigitToReverseArray(String digit) {
         int[] digits = new int[digit.length()];
-        for (int i = digit.length() - 1; i >= 0; i--) {
-            digits[i] = Character.getNumericValue(digit.charAt(i));
+        for (int i = digit.length() - 1, j = 0; i >= 0; i--) {
+            digits[i] = Character.getNumericValue(digit.charAt(j++));
         }
         return digits;
     }
